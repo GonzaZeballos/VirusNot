1b60383ad5d52f3d80709951a01a76ea2fb7d362
shafnash <201904924@est.umss.edu>
2022-10-14 02:45:25 -0400

    preRegistro

--- basquet/src/app/app.module.ts
+++ basquet/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistroComponent } from './registro/registro.component';
+import { HeaderComponent } from './registro/header/header.component';
+import { CabeceraComponent } from './registro/cabecera/cabecera.component';
+import { BodyComponent } from './registro/body/body.component';
+import { FooterComponent } from './registro/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    HeaderComponent,
+    CabeceraComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
