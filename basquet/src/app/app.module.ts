90b544c9956c362bad54a572744c77e9c70bdc0c
shafnash <201904924@est.umss.edu>
2022-10-17 01:37:38 -0400

    EnrutamientosYRegistroUsuario
    Listos los enrutamientos y la interface de registro usuario funcionando

--- basquet/src/app/app.module.ts
+++ basquet/src/app/app.module.ts
@@ -8,6 +8,15 @@ import { HeaderComponent } from './registro/header/header.component';
 import { CabeceraComponent } from './registro/cabecera/cabecera.component';
 import { BodyComponent } from './registro/body/body.component';
 import { FooterComponent } from './registro/footer/footer.component';
+import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
+import { BodyRUComponent } from './registro-usuario/body-ru/body-ru.component';
+import { HeaderRUComponent } from './registro-usuario/header-ru/header-ru.component';
+import { FooterRUComponent } from './registro-usuario/footer-ru/footer-ru.component';
+import { CabeceraRuComponent } from './registro-usuario/cabecera-ru/cabecera-ru.component';
+import { RegistroEscenarioComponent } from './registro-escenario/registro-escenario.component';
+import { SolicitudesPreInscripcionComponent } from './solicitudes-pre-inscripcion/solicitudes-pre-inscripcion.component';
+import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +25,16 @@ import { FooterComponent } from './registro/footer/footer.component';
     HeaderComponent,
     CabeceraComponent,
     BodyComponent,
-    FooterComponent
+    FooterComponent,
+    RegistroUsuarioComponent,
+    BodyRUComponent,
+    HeaderRUComponent,
+    FooterRUComponent,
+    CabeceraRuComponent,
+    RegistroEscenarioComponent,
+    SolicitudesPreInscripcionComponent,
+    LoginComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
